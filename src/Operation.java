ed9ca6ee6f1fb0d70796aa01769ead2992decc4e
dekadri9 <adria.de.castro@estudiantat.upc.edu>
2021-10-29 16:11:16 +0200

    Upgrade using thread properties

--- src/Operation.java
+++ src/Operation.java
@@ -1,28 +1,16 @@
 public class Operation implements Runnable {
 
     private int id;
-    private Long threadNumber;
-
-    public int getId(){
-        return this.id;
-    }
 
 
     public Operation (int id){
         this.id = id;
     }
 
-    public void setThreadNumber(Long threadNumber){
-        this.threadNumber = threadNumber;
-    }
-
-    public Long getThreadNumber() {
-        return threadNumber;
-    }
 
     @Override
     public void run() {
-            System.out.println("Performing operation " + id + ".");
+            System.out.println("Performing operation " + id + " in thread number " + Thread.currentThread().getId() + ".");
 
             try {
                 Thread.sleep(1500);
