8bbaaccfd2a1a03a2cafc2decfce1c02a65eb631
dekadri9 <adria.de.castro@estudiantat.upc.edu>
2021-10-29 17:45:57 +0200

    Commented source code

--- src/MultiThreadingOperations.java
+++ src/MultiThreadingOperations.java
@@ -3,27 +3,42 @@ import java.util.concurrent.*;
 public class MultiThreadingOperations {
     public static void main (String[] args) throws InterruptedException {
 
+        //Initializing all 5 operations with their IDs
         Operation op1 = new Operation(1);
         Operation op2 = new Operation(2);
         Operation op3 = new Operation(3);
         Operation op4 = new Operation(4);
         Operation op5 = new Operation(5);
 
+        //Building a 3 thread Pool Executor to manage our threads
         ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(3);
 
+        //Begin the execution of operations that have no dependencies
         Future<?> t1 = executor.submit(op1);
         Future<?> t2 = executor.submit(op2);
-        executor.submit(op5);
+        Future<?> t3 = executor.submit(op5);
 
         try {
+            //Getting operations 1 and 2 and waiting for
+            //them to finish before we start the operation 3
             t1.get();
             t2.get();
 
+            //Start operation 3 with a free thread of the pool
             t1 = executor.submit(op3);
 
+            //Wait for operation 3 to finish
             t1.get();
 
-            executor.submit(op4);
+            //Start operation 4 with another free thread
+            t1 = executor.submit(op4);
+
+            //Making sure that every operation finished before
+            //shutting down the executor
+            t1.get();
+            t3.get();
+
+            //Shutting down executor
             executor.shutdown();
 
         } catch (ExecutionException e) {
--- src/Operation.java
+++ src/Operation.java
@@ -1,18 +1,23 @@
-import java.util.concurrent.ThreadPoolExecutor;
-
 public class Operation implements Runnable {
 
+    //Operation identifier
     private int id;
 
+    //Operation constructor
     public Operation (int id){
         this.id = id;
     }
-    
+
+    //Override of Runnable function run()
     @Override
     public void run() {
+
+            //Inform user that some operation is performing and which thread is doing it
             System.out.println("Performing operation " + id + " in thread number " + Thread.currentThread().getId() + ".");
 
             try {
+                //Giving different "operation time" to some operations
+                //in order to see if constraints are met
                 if (id == 1){
                     Thread.sleep(500);
                 }
@@ -20,10 +25,12 @@ public class Operation implements Runnable {
                     Thread.sleep(4700);
                 }
                 else Thread.sleep(1500);
+
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
+            //Inform user about operation finishing and the release of its thread
             System.out.println("Operation " + id + " finished. Thread number " + Thread.currentThread().getId() + " released." );
 
     }
